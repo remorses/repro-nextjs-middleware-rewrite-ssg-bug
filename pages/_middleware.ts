ee39002e2bcec379c821d65662885f2b35b4b87d
remorses <beats.by.morse@gmail.com>
2022-02-03 18:44:22 +0100

    base apth

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,18 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export default function middleware(req: NextRequest) {
     const { pathname, basePath } = req.nextUrl
-    console.log({ basePath })
 
-    const hostname = req.headers.get('host')?.replace(/:\d+$/, '')
-
-    // does not work
-    // new URL(`/_hosts/${hostname}${pathname}`,req.nextUrl.origin,).toString()
+    const hostname = req.headers.get('host')
 
     // does not work
     // `/_hosts/${hostname}${pathname}`
-
-    // works!
-    // new URL(`${basePath}/_hosts/${hostname}${pathname}`, req.nextUrl.origin, ).toString()
 
     return NextResponse.rewrite(
         new URL(
