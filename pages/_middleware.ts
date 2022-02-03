73e787de28438b59235076e198c5cc517ad001f1
remorses <beats.by.morse@gmail.com>
2022-02-03 18:44:42 +0100

    pages/_middleware:

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export default function middleware(req: NextRequest) {
     const { pathname, basePath } = req.nextUrl
 
-    const hostname = req.headers.get('host')
+    const hostname = req.headers.get('host')?.replace(/:\d+$/, '')
 
     // does not work
     // `/_hosts/${hostname}${pathname}`
