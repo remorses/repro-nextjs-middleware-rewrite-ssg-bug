787e204d44baf71e186ddbccb274245c9ec17bd5
remorses <beats.by.morse@gmail.com>
2022-02-05 11:32:32 +0100

    pages/_middleware: try fixing

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export default function middleware(req: NextRequest) {
-    const { pathname, basePath } = req.nextUrl
+    const { pathname, basePath, locale, defaultLocale } = req.nextUrl
 
     const hostname = req.headers.get('host')?.replace(/:\d+$/, '')
 
     // does not work
     // `/_hosts/${hostname}${pathname}`
 
+    const localePath = locale === defaultLocale ? '' : `/${locale}`
     return NextResponse.rewrite(
         new URL(
-            `${basePath}/_hosts/${hostname}${pathname}`,
+            `${basePath}${localePath}/_hosts/${hostname}${pathname}`,
             req.nextUrl.origin,
         ).toString(),
     )
