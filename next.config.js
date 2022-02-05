7162a0c7952f39062b06a0fa4f8cf1269aad6f5b
remorses <beats.by.morse@gmail.com>
2022-02-05 11:30:34 +0100

    try locales

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    basePath: '/app',
+    // basePath: '/app',
+    i18n: {
+        locales: ['en', 'fr', 'es'],
+        defaultLocale: 'en',
+    },
 }
 
 module.exports = nextConfig
--- pages/_hosts/[host]/index.tsx
+++ pages/_hosts/[host]/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
 
 export const getStaticPaths: GetStaticPaths = async (context) => {
     return {
@@ -9,15 +10,34 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
     const host = context.params!.host
-
-    return { props: { host }, revalidate: 1 }
+    const locale = context.locale
+    return { props: { host, locale }, revalidate: 1 }
 }
 export default function Page({ host }) {
+    const router = useRouter()
+    const { locale, defaultLocale, pathname, asPath } = router
     return (
         <div>
             <h1>
-                Your host is <code>{host}</code>
+                Your host is <code>{host}</code> and your locale is{' '}
+                <code>{locale}</code>
             </h1>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <button
+                    onClick={() => {
+                        router.push({ pathname }, asPath, { locale: 'fr' })
+                    }}
+                >
+                    set locale to fr
+                </button>
+                <button
+                    onClick={() => {
+                        router.push({ pathname }, asPath, { locale: 'en' })
+                    }}
+                >
+                    set locale to en
+                </button>
+            </div>
         </div>
     )
 }
