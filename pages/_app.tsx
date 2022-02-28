4d0bfd8a3c7680a3de811109bf588f951f970fc3
remorses <beats.by.morse@gmail.com>
2022-02-28 20:24:52 +0100

    show if page reloads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <div className=''>
+            <C />
+            <Component {...pageProps} />
+        </div>
+    )
+}
+
+function C() {
+    const [state, setState] = useState(0)
+    useEffect(() => {
+        setState((x) => x + 1)
+    }, [])
+    return <div className=''>{state}</div>
 }
 
 export default MyApp
